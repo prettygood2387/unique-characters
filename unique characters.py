23e7c7416c7ccafd006ec4f8b763a14ca17810d6
prettygood2387 <139783538+prettygood2387@users.noreply.github.com>
2023-07-31 00:11:15 +0100

    Add files via upload

---
+++ unique characters.py
@@ -0,0 +1,12 @@
+import random
+lower = 'abcdertwqazxcbnmkopuiyglur'
+upper = lower.upper()
+special = '!@#$%^&*()_+<>?/:;'
+digits = '123456789'
+all = lower + upper + special + digits
+
+password = ""
+
+for m in range(1,16):
+    password = "".join([password,random.choice(all)])
+print(password)
