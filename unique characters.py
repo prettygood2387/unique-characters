cc49497c8c2417b6edb4714576f93fb9bf14ea1b
prettygood2387 <139783538+prettygood2387@users.noreply.github.com>
2023-07-31 00:22:54 +0100

    Update unique characters.py

--- unique characters.py
+++ unique characters.py
@@ -1,12 +1,13 @@
-import random
-lower = 'abcdertwqazxcbnmkopuiyglur'
-upper = lower.upper()
-special = '!@#$%^&*()_+<>?/:;'
-digits = '123456789'
-all = lower + upper + special + digits
+if __name__ = '__main__':
+    import random
+    lower = 'abcdertwqazxcbnmkopuiyglur'
+    upper = lower.upper()
+    special = '!@#$%^&*()_+<>?/:;'
+    digits = '123456789'
+    all = lower + upper + special + digits
 
-password = ""
+    password = ""
 
-for m in range(1,16):
-    password = "".join([password,random.choice(all)])
-print(password)
+    for m in range(1,16):
+        password = "".join([password,random.choice(all)])
+    print(password)
